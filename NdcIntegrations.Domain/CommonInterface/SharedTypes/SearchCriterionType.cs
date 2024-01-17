2cfd275b9f44450ab47da122ea79ad8944161c09
Raouf-Mohammed <raouf@shuratech.com>
2024-01-17 10:43:55 +0200

    - Added segment details. - Changed lists and arrays into ICollections. - Changed domain into core.

--- NdcIntegrations.Domain/CommonInterface/SharedTypes/SearchCriterionType.cs
+++ NdcIntegrations.Domain/CommonInterface/SharedTypes/SearchCriterionType.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace NdcIntegrations.Domain.CommonInterface
+namespace NdcIntegrations.Core.CommonInterface
 {
     public class SearchCriterionType
     {
